03bb41d2e0592b851592124067724229457e572e
Dishant-Shah-Projects <61332472+Dishant-Shah-Projects@users.noreply.github.com>
2020-05-12 17:17:27 -0500

    Add files via upload
    changed table information for rating

--- tablecreator2.sql
+++ tablecreator2.sql
@@ -5,6 +5,7 @@
 -- create database antdishi_userstorage;
 -- use antdishi_userstorage;
  -- rating and visit amount table for each page   
+-- sample table creating  code
 create table review
 ( pname varchar(15),
 Purl varchar (100),
@@ -18,178 +19,57 @@ fivestar numeric(5,0),
 Average numeric(5,0),
 primary key (pname)
 );
--- review table for each page
-create table dp1
+-- Step #1 Insert products into the review page.
+-- insertion query (pname, purl, pimage, 0,0,0,0,0,0,0); for each product
+-- example 
+-- insert into review values('dsp1','purl','pimageurl',0,0,0,0,0,0,0);
+
+
+-- review table for each produce. 
+-- Name them for  your initials and p#. 
+-- example below. 
+create table dsp1
 (review varchar(200),
 star varchar (30),
 primary key(review));
-create table dp2
+create table dsp2
 (review varchar(200),
 star varchar (30),
 primary key(review));
-create table dp3
+create table dsp3
+(review varchar(200),
+star varchar (30),
+primary key(review,star));
+create table dsp4
 (review varchar(200),
 star varchar (30),
 primary key(review));
-create table dp4
+create table dsp5
 (review varchar(200),
 star varchar (30),
 primary key(review));
-create table dp5
+create table dsp6
 (review varchar(200),
 star varchar (30),
 primary key(review));
-create table dp6
+create table dsp7
 (review varchar(200),
 star varchar (30),
 primary key(review));
-create table dp7
+create table dsp8
 (review varchar(200),
 star varchar (30),
 primary key(review));
-create table dp8
+create table dsp9
 (review varchar(200),
 star varchar (30),
 primary key(review));
-create table dp9
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table dp10
+create table dsp10
 (review varchar(200),
 star varchar (30),
 primary key(review));
 
 
-create table pp1
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp2
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp3
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp4
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp5
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp6
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp7
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp8
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp9
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table pp10
-(review varchar(200),
-star varchar (30),
-primary key(review));
-
-create table rmp1
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp2
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp3
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp4
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp5
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp6
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp7
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp8
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp9
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rmp10
-(review varchar(200),
-star varchar (30),
-primary key(review));
-
-
-
-
-create table rap1
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap2
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap3
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap4
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap5
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap6
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap7
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap8
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap9
-(review varchar(200),
-star varchar (30),
-primary key(review));
-create table rap10
-(review varchar(200),
-star varchar (30),
-primary key(review));
-
-
--- insertion query (pname, purl, pimage, 0,0,0,0,0,0,0);
--- insert into review values('dp1',0,0,0,0,0,0,0);
 
 
 
